2a5a0e80c4a224bbc6ff0f8bdcde010acb205ced
scottx611x <ouellettes1@wit.edu>
2015-08-07 18:12:56 -0400

    finished pyChallenge 2

--- pyChallenge2.py
+++ pyChallenge2.py
@@ -1,3 +1,19 @@
 __author__ = 'Scott'
-#pyChallenge 2
-#http://www.pythonchallenge.com/pc/def/ocr.html
+# pyChallenge 2
+# http://www.pythonchallenge.com/pc/def/ocr.html
+
+import re
+
+with open('trash1.txt', "r") as f:
+    new = ""
+    for line in f:
+        x = re.findall(r'[^+-_\^\[\]!@#$&*(){} % \n]', line, flags=0)
+        if x:
+            new += x[0]
+
+    print new
+
+# OUTPUT
+# equality
+#url
+# http://www.pythonchallenge.com/pc/def/equality.html
