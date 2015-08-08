0cb08bb6a56011c8ceb3ed730a263ea161e57900
scottx611x <ouellettes1@wit.edu>
2015-08-07 23:55:36 -0400

    pyChallenge7 complete

--- pyChallenge7.py
+++ pyChallenge7.py
@@ -1,3 +1,11 @@
 __author__ = 'Scott'
-#pyChallenge 6
-#url: http://www.pythonchallenge.com/pc/def/channel.html
+#pyChallenge 7
+#url: http://www.pythonchallenge.com/pc/def/oxygen.html
+import re
+from PIL import Image
+
+i = Image.open("oxygen.png") # http://www.pythonchallenge.com/pc/def/oxygen.png
+row = [i.getpixel((x, 45)) for x in range(0, i.size[0], 7)]
+ords = [r for r, g, b, a in row if r == g == b]
+
+print "".join(map(chr, map(int, re.findall("\d+", "".join(map(chr, ords))))))
